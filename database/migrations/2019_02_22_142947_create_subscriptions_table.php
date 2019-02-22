cc072244acd2626e36297d78f3af431b33ef4d35
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-02-22 15:50:29 +0100

    Added new migrations

---
+++ database/migrations/2019_02_22_142947_create_subscriptions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->smallInteger('type');
+            $table->string('account_number');
+            $table->string('payment_provider');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->smallInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
